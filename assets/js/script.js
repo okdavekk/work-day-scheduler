c7bf1a7b0abfcc6d19c504fcea9a4b8a168f72e9
okdavekk <okdavekk@gmail.com>
2022-05-29 14:48:47 -0400

    Updated Readme.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 
 //Current day display
 var currentDayEl = $("currentDay")
-var currentDate = moment().format("DD MMMM YYYY")
+var currentDate = moment().format("DD MMMM YYYY, HH:HH")
 $("#currentDay").html(currentDate);
 
 
