83e7a5f79f9601c608abc2117df58cf8d4f2f9b0
okdavekk <okdavekk@gmail.com>
2022-05-29 15:07:50 -0400

    Added Ids to table rows.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,18 @@
 
-//Current day display
+//Current day display variables
 var currentDayEl = $("currentDay")
-var currentDate = moment().format("DD MMMM YYYY, HH:HH")
+var currentDate = moment().format("DD MMMM YYYY")
+
+//Variables to change colors of row if in the past or present time
+var deductTimeColor = moment().startOf('hour').fromNow();
+var addTimeColor = moment().startOf('hour').fromNow();
+
+
+
+//Current day display write to HTML
 $("#currentDay").html(currentDate);
 
 
+
+
+console.log();
