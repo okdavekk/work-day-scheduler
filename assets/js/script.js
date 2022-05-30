1811eb8759fafc63e5d56666756181112215cd13
okdavekk <okdavekk@gmail.com>
2022-05-30 08:14:05 -0400

    Good time to push after lots of changes.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,9 @@ var currentTime = moment().format("HH")
 
 
 
-//Variables to change colors of row if in the past or present time
-var deductTimeColor = moment().startOf('hour').fromNow();
-var addTimeColor = moment().startOf('hour').fromNow();
+// //Variables to change colors of row if in the past or present time
+// var deductTimeColor = moment().startOf("hour").fromNow();
+// var addTimeColor = moment().startOf("hour").fromNow();
 
 
 
@@ -16,91 +16,146 @@ var addTimeColor = moment().startOf('hour').fromNow();
 $("#currentDay").html(currentDate);
 
 
-//Row color change for time past... Goes to red when its past
-if (currentTime > 9){
-    $("#0900-row").css("color", "red");
+//Text areas
+
+
+
+
+
+//REFACTOR
+//REFACTOR
+//REFACTOR
+//Row color change for time past... Goes to white when its past
+if (currentTime > 9) {
+    $("#0900-row").css("background-color", "#d3d3d3");
 }
 
-if (currentTime > 10){
-    $("#1000-row").css("color", "red");
+if (currentTime > 10) {
+    $("#1000-row").css("background-color", "#d3d3d3");
 }
 
-if (currentTime > 11){
-    $("#1100-row").css("color", "red");
+if (currentTime > 11) {
+    $("#1100-row").css("background-color", "#d3d3d3");
 }
 
-if (currentTime > 12){
-    $("#1200-row").css("color", "red");
+if (currentTime > 12) {
+    $("#1200-row").css("background-color", "#d3d3d3");
 }
 
-if (currentTime > 13){
-    $("#1300-row").css("color", "red");
+if (currentTime > 13) {
+    $("#1300-row").css("background-color", "#d3d3d3");
 }
 
-if (currentTime > 14){
-    $("#1400-row").css("color", "red");
-}
-
-
-if (currentTime > 15){
-    $("#1500-row").css("color", "red");
-}
-
-
-if (currentTime > 16){
-    $("#1600-row").css("color", "red");
-}
-
-if (currentTime > 17){
-    $("#1700-row").css("color", "red");
+if (currentTime > 14) {
+    $("#1400-row").css("background-color", "#d3d3d3");
 }
 
 
+if (currentTime > 15) {
+    $("#1500-row").css("background-color", "#d3d3d3");
+}
 
+
+if (currentTime > 16) {
+    $("#1600-row").css("background-color", "#d3d3d3");
+}
+
+if (currentTime > 17) {
+    $("#1700-row").css("background-color", "#d3d3d3");
+}
+
+// //Row color change for time past... Goes to red when its the present time
+// if (currentTime = 9) {
+//     $("#0900-row").css("background-color", "#ff6961");
+// }
+
+// if (currentTime = 10) {
+//     $("#1000-row").css("background-color", "#ff6961");
+// }
+
+// if (currentTime = 11) {
+//     $("#1100-row").css("background-color", "#ff6961");
+// }
+
+// if (currentTime = 12) {
+//     $("#1200-row").css("background-color", "#ff6961");
+// }
+
+// if (currentTime = 13) {
+//     $("#1300-row").css("background-color", "#ff6961");
+// }
+
+// if (currentTime = 14) {
+//     $("#1400-row").css("background-color", "#ff6961");
+// }
+
+
+// if (currentTime = 15) {
+//     $("#1500-row").css("background-color", "#ff6961");
+// }
+
+
+// if (currentTime = 16) {
+//     $("#1600-row").css("background-color", "#ff6961");
+// }
+
+// if (currentTime = 17) {
+//     $("#1700-row").css("background-color", "#ff6961");
+// }
+
+
+//REFACTOR
+//REFACTOR
+//REFACTOR
 //Row color change for time to come... Goes to green when its in the future
-if (currentTime < "9"){
-    $("#0900-row").css("color", "green");
+if (currentTime < 9) {
+    $("#0900-row").css("background-color", "#77dd77");
 }
 
-if (currentTime < 10){
-    $("#1000-row").css("color", "green");
+if (currentTime < 10) {
+    $("#1000-row").css("background-color", "#77dd77");
 }
 
-if (currentTime < 11){
-    $("#1100-row").css("color", "green");
+if (currentTime < 11) { 
+    $("#1100-row").css("background-color", "#77dd77");
 }
 
-if (currentTime < 12){
-    $("#1200-row").css("color", "green");
+if (currentTime < 12) {
+    $("#1200-row").css("background-color", "#77dd77");
 }
 
-if (currentTime < 13){
-    $("#1300-row").css("color", "green");
+if (currentTime < 13) {
+    $("#1300-row").css("background-color", "#77dd77");
 }
 
-if (currentTime < 14){
-    $("#1400-row").css("color", "green");
+if (currentTime < 14) {
+    $("#1400-row").css("background-color", "#77dd77");
 }
 
-if (currentTime < 15){
-    $("#1500-row").css("color", "green");
+if (currentTime < 15) {
+    $("#1500-row").css("background-color", "#77dd77");
 }
 
-
-if (currentTime < 16){
-    $("#1600-row").css("color", "green");
+if (currentTime < 16) {
+    $("#1600-row").css("background-color", "#77dd77");
 }
 
-if (currentTime < 17){
-    $("#1700-row").css("color", "green");
+if (currentTime < 17) {
+    $("#1700-row").css("background-color", "#77dd77");
 }
 
 
 
 
 //Click events
-$("#0900-save-button").on("click", function() {
-    alert("hi");
+$("#0900-save-button").on("click", function(event) {
+    event.preventDefault()
+
+    var nineTextBox = $("textarea#0900-text-area").val();
+
+    console.log(nineTextBox);
+
+    localStorage.setItem("0900", nineTextBox);
 });
 
 $("#1000-save-button").on("click", function() {
@@ -143,7 +198,7 @@ $("#1700-save-button").on("click", function() {
 
 
 
-
+//Local Storage
 
 
 
